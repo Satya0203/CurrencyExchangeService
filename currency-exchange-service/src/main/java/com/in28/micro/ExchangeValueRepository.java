05dc0b856e42b54f668311d8c6668b01c4296b1e
Satya0203 <satyasreeram0203@gmail.com>
2020-05-07 13:03:42 +0530

    First Commit

---
+++ currency-exchange-service/src/main/java/com/in28/micro/ExchangeValueRepository.java
@@ -0,0 +1,10 @@
+package com.in28.micro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long>  
+{  
+	//creating query method  
+	ExchangeValue findByFromAndTo(String from, String to);  
+
+}
